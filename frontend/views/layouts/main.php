1b38abfe1e0e340181cb3ab8fd91d3d83d5a4351
Alex <alexey.dyakonov@catenamedia.com>
2019-11-08 14:30:52 +0200

    Added binct class for mobile numbers

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -127,9 +127,9 @@ $contacts = \backend\models\Contact::getContact();
 		<div class="mobile-contact">
 			<a href="#contact-opener"><img data-src="/img/icon/phone.svg" width="19" alt=""> <?= BaseController::getMessage('8') ?></a>
 			<ul class="mobile-contact-list">
-				<li><a href="tel:<?= $contacts->phone; ?>"><?= $contacts->phone; ?></a></li>
-				<li><a href="tel:<?= $contacts->mobile_1; ?>"><?= $contacts->mobile_1; ?></a></li>
-				<li><a href="tel:<?= $contacts->mobile_2; ?>"><?= $contacts->mobile_2; ?></a></li>
+				<li><a class="binct-phone-number-1" href="tel:<?= $contacts->phone; ?>"><?= $contacts->phone; ?></a></li>
+				<li><a class="binct-phone-number-2" href="tel:<?= $contacts->mobile_1; ?>"><?= $contacts->mobile_1; ?></a></li>
+				<li><a class="binct-phone-number-3" href="tel:<?= $contacts->mobile_2; ?>"><?= $contacts->mobile_2; ?></a></li>
 			</ul>
 		</div>
 		<div id="nav-icon">
