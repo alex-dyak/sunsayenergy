43845da31e01163b3f3100cd50b1523efae7ef48
julia-karpenko <julia.karpenko@catenamedia.com>
2019-12-04 11:11:11 +0200

    updated form

--- frontend/views/section/_connect.php
+++ frontend/views/section/_connect.php
@@ -1,36 +1,25 @@
 <?php use frontend\controllers\BaseController; ?>
 
 <section class="section connect" id="connect">
-    <div class="connect__content">
-        <div class="connect__content-info">
-            <div class="connect__content-title line-title">
-                <h2><span><?= BaseController::getMessage('364') ?></span> </h2>
-            </div>
-            <form class="connect__content-question" data-post-url="index.html">
-                <div class="message">
-
-                    <input class="custom-field" type="text" name="fullname" placeholder="<?= BaseController::getMessage('366') ?>">
-
-                    <input class="custom-field" id="phone" type="text" name="phone" placeholder="+380">
-
-                    <input class="custom-field" type="text" name="email" placeholder="youremail@mail.com">
+	<div class="container">
+		<div class="connect__content">
+			<div class="connect__content-title line-title">
+				<h2><span><?= BaseController::getMessage('364') ?></span> </h2>
+			</div>
+			<form class="connect__content-question" data-post-url="index.html">
+				<div class="message">
+					<input class="custom-field" type="text" name="fullname" placeholder="<?= BaseController::getMessage('366') ?>">
+					<input class="custom-field" id="phone" type="text" name="phone" placeholder="+380">
+					<input class="custom-field" type="text" name="email" placeholder="youremail@mail.com">
 					<textarea class="custom-field" name="question" id="question" rows="5" placeholder="<?= BaseController::getMessage('403') ?>"></textarea>
-                    <div class="connect__content-button">
+					<div class="connect__content-button">
 						<button class="btn connect-btn btn-primary">
 							<?= BaseController::getMessage('14') ?>
 						</button>
 						<span class="form-compelete"><?= BaseController::getMessage('374') ?></span>
 					</div>
-                </div>
-            </form>
-        </div>
-        <div class="connect__content-image">
-            <?php if ( !BaseController::is_mobile()  ): ?>
-                <picture>
-                    <source data-srcset="/img/connect.webp" type="image/webp">
-                    <img data-src="/img/connect.jpg" alt="">
-                </picture>                
-            <?php endif; ?>
-        </div>
-    </div>
+				</div>
+			</form>
+		</div>
+	</div>
 </section>
