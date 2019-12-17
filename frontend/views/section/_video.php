10c4063852af101ccdcb101ffe5b99708ffe017b
Alex <alexey.dyakonov@catenamedia.com>
2019-12-17 14:59:31 +0200

    Fixed video amount

--- frontend/views/section/_video.php
+++ frontend/views/section/_video.php
@@ -4,17 +4,21 @@
     <div class="container">
         <h2><?= BaseController::getMessage('412') ?></h2>
         <div class="video-row">
-            <?php foreach ($videos as $item): ?>
-                <div class="video-col">
-                    <div class="embed-responsive">
-                        <iframe src="https://www.youtube.com/embed/<?= $item->video_url ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <?php
+            $i = 0;
+            foreach ($videos as $item): ?>
+                <?php if($i <= 3) : ?>
+                    <div class="video-col">
+                        <div class="embed-responsive">
+                            <iframe src="https://www.youtube.com/embed/<?= $item->video_url ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                        <strong class="video-title"><?= $item->video_name ?></strong>
                     </div>
-                    <strong class="video-title"><?= $item->video_name ?></strong>
-                </div>
-            <?php endforeach; ?>
+                <?php endif; ?>
+            <?php $i++; endforeach; ?>
         </div>
         <div class="btn-wrap">
-            <a href="<?= \yii\helpers\Url::to(['/video']); ?>" class="button"><?= BaseController::getMessage('413') ?></a>
+            <a href="<?= \yii\helpers\Url::to(['/video']); ?>" class="button"><?= BaseController::getMessage('419') ?></a>
         </div>
     </div>
 </section>
