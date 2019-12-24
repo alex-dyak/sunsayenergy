fc45ca69d9fd606440498ea8eba5bfaefc9c1fc7
julia-karpenko <julia.karpenko@catenamedia.com>
2019-12-24 15:46:27 +0200

    added utp blocks

---
+++ frontend/views/section/_advantages.php
@@ -0,0 +1,30 @@
+<?php use frontend\controllers\BaseController; ?>
+
+<section class="advantages-section">
+    <div class="container">
+        <div class="title-block">
+            <h2 class="line-title"><span><?= BaseController::getMessage('420') ?></span></h2>
+            <div class="img-block">
+                <img data-src="/img/houses.jpg" alt="houses">
+            </div>
+            <div class="text-block">
+                <p><?= BaseController::getMessage('421') ?></p>
+                <a class="button btn-lg" href="#connect"><?= BaseController::getMessage('402') ?></a>
+            </div>
+        </div>
+        <ul class="advantages-list">
+            <li>
+                <strong class="line-title"><span><?= BaseController::getMessage('422') ?></span></strong>
+                <p><?= BaseController::getMessage('423') ?></p>
+            </li>
+            <li>
+                <strong class="line-title"><span><?= BaseController::getMessage('424') ?></span></strong>
+                <p><?= BaseController::getMessage('425') ?></p>
+            </li>
+            <li>
+                <strong class="line-title"><span><?= BaseController::getMessage('426') ?></span></strong>
+                <p><?= BaseController::getMessage('427') ?></p>
+            </li>
+        </ul>
+    </div>
+</section>
