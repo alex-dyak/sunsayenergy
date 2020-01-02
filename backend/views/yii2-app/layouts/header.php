341eda1311bc0830563e7abd9e6b2e88e6a84802
julia-karpenko <julia.karpenko@catenamedia.com>
2020-01-02 11:05:00 +0200

    fixed header in admin

--- backend/views/yii2-app/layouts/header.php
+++ backend/views/yii2-app/layouts/header.php
@@ -12,7 +12,7 @@ use yii\helpers\Html;
     <a class="logo" href="/" style="background-image: url(/img/logo.png);background-size: 50%;background-repeat: no-repeat;background-position: center;"><span class="logo-mini">APP</span><span class="logo-lg"></span></a>
     <nav class="navbar navbar-static-top" role="navigation">
 
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
             <span class="sr-only">Toggle navigation</span>
         </a>
         <ul class="nav navbar-nav" style="float:right">
