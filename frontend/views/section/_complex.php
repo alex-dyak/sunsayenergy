f43942aff47411b90a32d477b92ed4efb9c73be3
julia-karpenko <julia.karpenko@catenamedia.com>
2020-01-22 19:10:47 +0200

    made blocks smaller

--- frontend/views/section/_complex.php
+++ frontend/views/section/_complex.php
@@ -16,19 +16,19 @@
 					</div>
 					<ul class="compare-list">
 						<li class="pros">
-							<img src="./img/complex/pros.svg" alt="pros" width="19">
+							<img src="/img/complex/pros.svg" alt="pros" width="19">
 							<?= BaseController::getMessage('389') ?>
 						</li>
 						<li class="pros">
-							<img src="./img/complex/pros.svg" alt="pros" width="19">
+							<img src="/img/complex/pros.svg" alt="pros" width="19">
 							<?= BaseController::getMessage('390') ?>
 						</li>
 						<li class="pros">
-							<img src="./img/complex/pros.svg" alt="pros" width="19">
+							<img src="/img/complex/pros.svg" alt="pros" width="19">
 							<?= BaseController::getMessage('391') ?>
 						</li>
 						<li class="cons">
-							<img src="./img/complex/cons.svg" alt="cons" width="16">
+							<img src="/img/complex/cons.svg" alt="cons" width="16">
 							<?= BaseController::getMessage('443') ?>
 						</li>
 					</ul>
@@ -42,19 +42,19 @@
 					</div>
 					<ul class="compare-list">
 						<li class="pros">
-							<img src="./img/complex/pros.svg" alt="pros" width="19">
+							<img src="/img/complex/pros.svg" alt="pros" width="19">
 							<?= BaseController::getMessage('326') ?>
 						</li>
 						<li class="pros">
-							<img src="./img/complex/pros.svg" alt="pros" width="19">
+							<img src="/img/complex/pros.svg" alt="pros" width="19">
 							<?= BaseController::getMessage('327') ?>
 						</li>
 						<li class="pros">
-							<img src="./img/complex/pros.svg" alt="pros" width="19">
+							<img src="/img/complex/pros.svg" alt="pros" width="19">
 							<?= BaseController::getMessage('328') ?>
 						</li>
 						<li class="cons">
-							<img src="./img/complex/cons.svg" alt="cons" width="16">
+							<img src="/img/complex/cons.svg" alt="cons" width="16">
 							<?= BaseController::getMessage('444') ?>
 						</li>
 					</ul>
@@ -68,19 +68,19 @@
 					</div>
 					<ul class="compare-list">
 						<li class="pros">
-							<img src="./img/complex/pros.svg" alt="pros" width="19">
+							<img src="/img/complex/pros.svg" alt="pros" width="19">
 							<?= BaseController::getMessage('394') ?>
 						</li>
 						<li class="pros">
-							<img src="./img/complex/pros.svg" alt="pros" width="19">
+							<img src="/img/complex/pros.svg" alt="pros" width="19">
 							<?= BaseController::getMessage('395') ?>
 						</li>
 						<li class="pros">
-							<img src="./img/complex/pros.svg" alt="pros" width="19">
+							<img src="/img/complex/pros.svg" alt="pros" width="19">
 							<?= BaseController::getMessage('396') ?>
 						</li>
 						<li class="cons">
-							<img src="./img/complex/cons.svg" alt="cons" width="16">
+							<img src="/img/complex/cons.svg" alt="cons" width="16">
 							<?= BaseController::getMessage('445') ?>
 						</li>
 					</ul>
