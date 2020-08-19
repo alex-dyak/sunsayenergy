8f6dfef3f0fcf001da0586e8cc2678f1a50462db
julia-karpenko <julia.karpenko@catenamedia.com>
2020-08-19 14:20:26 +0300

    added warranty block

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -115,62 +115,76 @@ $contacts = \backend\models\Contact::getContact();
 			</ol>
 		</div>
 	</section>
-
-    <section class="section about">
-        <div class="about__title">
-            <h1><?= BaseController::getMessage('76') ?></h1>
-        </div>
-        <div class="about__description">
-            <p><?= BaseController::getMessage('77') ?></p>
-            <picture>
-                <source media="(min-width: 600px)" data-srcset="/img/about/1.jpg" type="image/jpg">
-                <source media="(max-width: 599px)" data-srcset="/img/about/m_1.jpg" type="image/jpg">
-                <img data-src="/img/about/1.jpg" alt=""  class="about__image">
-            </picture>
-            <p><?= BaseController::getMessage('78') ?></p>
-            <p><?= BaseController::getMessage('79') ?></p>
-            <p><?= BaseController::getMessage('80') ?></p>
-            <ul>
-                <li><?= BaseController::getMessage('81') ?></li>
-                <li><?= BaseController::getMessage('82') ?></li>
-                <li><?= BaseController::getMessage('83') ?></li>
-            </ul>
-        </div>
-
-        <div class="about__image-list">
-            <div class="about__image-list__item">
-                <picture>
-                <source media="(min-width: 600px)" data-srcset="/img/about/2.jpg" type="image/jpg">
-                <source media="(max-width: 599px)" data-srcset="/img/about/m_2.jpg" type="image/jpg">
-                <img data-src="/img/about/2.jpg" alt="">
-            </picture>
-            </div>
-            <div class="about__image-list__item">
-                <picture>
-                <source media="(min-width: 600px)" data-srcset="/img/about/3.jpg" type="image/jpg">
-                <source media="(max-width: 599px)" data-srcset="/img/about/m_3.jpg" type="image/jpg">
-                <img data-src="/img/about/3.jpg" alt=""  class="about__image">
-            </picture>
-            </div>
-        </div>
-            
-        <div class="about__description">
-            <p class="about__center"><?= BaseController::getMessage('84') ?></p>
-            <div class="about__image-block">
-                <picture>
-                    <source media="(min-width: 600px)" data-srcset="/img/about/4.jpg" type="image/jpg">
-                    <source media="(max-width: 599px)" data-srcset="/img/about/m_4.jpg" type="image/jpg">
-                    <img data-src="/img/about/4.jpg" alt=""  class="about__image">
-                </picture>
-                <p class="about__image-description"><?= BaseController::getMessage('85') ?></p>
-            </div>
-        </div>
+	<section class="green-payment">
+		<div class="container">
+			<ul class="green-payment-list">
+				<li class="green-payment-item background-lazy" data-src="/img/commercial-station/warranty.jpg"></li>
+				<li class="green-payment-img">
+					<img data-src="/img/green/icon/check.svg" alt="<?= BaseController::getMessage('550') ?>" width="65">
+				</li>
+				<li class="green-payment-info">
+                    <?= BaseController::getMessage('578') ?>
+				</li>
+			</ul>
+		</div>
+	</section>
+    <section class="about-us-section">
+        <div class="container">
+			<div class="staff-description">
+				<p><?= BaseController::getMessage('579') ?></p>
+			</div>
+<div class="staff-slider">
+	<div class="slide">
+		<div class="img-block">
+			<img data-src="/img/about/1.jpg" alt="<?= BaseController::getMessage('580') ?>">
+		</div>
+		<div class="staff-info">
+			<strong><?= BaseController::getMessage('580') ?></strong>
+			<p><?= BaseController::getMessage('581') ?></p>
+		</div>
+	</div>
+	<div class="slide">
+		<div class="img-block">
+			<img data-src="/img/about/2.jpg" alt="<?= BaseController::getMessage('580') ?>">
+		</div>
+		<div class="staff-info">
+			<strong><?= BaseController::getMessage('580') ?></strong>
+			<p><?= BaseController::getMessage('581') ?></p>
+		</div>
+	</div>
+	<div class="slide">
+		<div class="img-block">
+			<img data-src="/img/about/3.jpg" alt="<?= BaseController::getMessage('580') ?>">
+		</div>
+		<div class="staff-info">
+			<strong><?= BaseController::getMessage('580') ?></strong>
+			<p><?= BaseController::getMessage('581') ?></p>
+		</div>
+	</div>
+	<div class="slide">
+		<div class="img-block">
+			<img data-src="/img/about/4.jpg" alt="<?= BaseController::getMessage('580') ?>">
+		</div>
+		<div class="staff-info">
+			<strong><?= BaseController::getMessage('580') ?></strong>
+			<p><?= BaseController::getMessage('581') ?></p>
+		</div>
+	</div>
+	<div class="slide">
+		<div class="img-block">
+			<img data-src="/img/about/3.jpg" alt="<?= BaseController::getMessage('580') ?>">
+		</div>
+		<div class="staff-info">
+			<strong><?= BaseController::getMessage('580') ?></strong>
+			<p><?= BaseController::getMessage('581') ?></p>
+		</div>
+	</div>
+</div>
+		</div>
     </section>
-    <!-- start review -->
-   <? /* echo $this->render('../section/_review.php', compact('reviews')); */ ?>
-	<!-- end review -->
+
     <!-- start connect -->
-    <?= $this->render('../section/_connect.php'); ?>
+    <?= $this->render('../section/_connect-blue.php'); ?>
 	<!-- end connect -->
 </main>
 
