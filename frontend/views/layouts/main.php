547e1722f5f61bc0ba37d3fb2be89050b6c9c499
Alex <alexey.dyakonov@catenamedia.com>
2020-12-07 13:08:36 +0200

    Changed autoopen param

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -99,7 +99,7 @@ $contacts = \backend\models\Contact::getContact();
                 host: '//quiz.marquiz.ru',
                 id: '5fa185757b366e00441631e0',
                 autoOpen: 80,
-                autoOpenFreq: 'always',
+                autoOpenFreq: '',
                 openOnExit: false
             });
         });
