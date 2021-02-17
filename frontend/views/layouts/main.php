5818b67f56e98800ad3f7c289028407ef4ee8085
Alex <alexey.dyakonov@catenamedia.com>
2021-02-17 12:25:23 +0200

    Added google analitics script

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -697,7 +697,16 @@ $contacts = \backend\models\Contact::getContact();
           complete: function (response) {
             $(".quiz-step").hide();
             $(".quiz-final-screen").css("display", "flex").animate({opacity: '1'}, 300, "swing");
-            ga('send', 'event', 'quiz_form', 'submit', 'quiz');
+              <!-- Google Analytics -->
+                  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+              ga('create', ' UA-131310740-1', 'auto');
+              ga('send', 'event', 'quiz_form', 'submit', 'quiz');
+            <!-- End Google Analytics -->
+
             setTimeout(function(){
               $('.quiz-modal').fadeOut(300);
               sessionStorage.setItem('isVisibleQuiz', 'false');
