b9afb155b137e48ac0c6a7f50ca7858be706b1e0
annadobriak <dobriakanna@gmail.com>
2021-05-26 16:42:25 +0300

    Added styles for new landing page

--- frontend/views/site/landing.php
+++ frontend/views/site/landing.php
@@ -7,19 +7,52 @@ $this->params['breadcrumbs'][] = [
     'label' => BaseController::getMessage('5'),
 ];
 
+
+
+
 $contacts = \backend\models\Contact::getContact();
 ?>
     <main class="page-container">
 		<div class="breadcrumbs-wrap">
 			<div class="container">
+ 
                 <?php echo Breadcrumbs::widget([
                     'homeLink' => ['label' => BaseController::getMessage('404'), 'url' => Yii::$app->homeUrl],
                     'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
                 ]); ?>
 			</div>
 		</div>
+ <!-- start info -->
+   
+    <section class="dohod-zelyonyi-tarif-section" style="background-image: url('/img/dohod-zelyonyi-tarif/dohod-zelyonyi-bg.jpg')">
+        <div class="container">
+            <div class="dohod-zelyonyi-wrapper">
+                <div class="dohod-zelyonyi-inner">
+                    <h1 class="dohod-zelyonyi-title">
+                    СОЛНЕЧНЫЕ ЭЛЕКТРОСТАНЦИИ ПОД «ЗЕЛЕНЫЙ» ТАРИФ С ПРИБЫЛЬЮ ДО $6 500
+                    </h1>
+                    <p class="dohod-zelyonyi-advantages">
+                        Приносят стабильный доход от продажи сгенерированной электроэнергии
+                    </p>
+                    <p class="dohod-zelyonyi-advantages">
+                        Возвращают инвестиции от 4 лет, а дальше стабильно зарабатывают
+                    </p>
+                    <p class="dohod-zelyonyi-advantages">
+                        Работают 25 лет, поддерживаются SUNSAY Energy в течение всего срока
+                    </p>
 
-	<?php echo $message; ?>
+                </div>
+                <div class="dohod-zelyonyi-form">
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+   
+
+	<!-- <?php echo $message; ?> -->
 
 		<!-- start connect -->
         <?= $this->render('../section/_connect-blue.php'); ?>
