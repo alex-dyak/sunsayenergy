b37003fcd4014eeddf965da0d2d98edb91d677eb
Alex <alexey.dyakonov@catenamedia.com>
2021-11-04 14:13:35 +0200

    Created migration for SEO model. Added new fields for SEO article

--- backend/views/seo/_form.php
+++ backend/views/seo/_form.php
@@ -8,37 +8,74 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 <?php
-$fieldCode = "Title";
+$fieldCode = "seo_title";
+$fieldLabel = "SEO Title";
 $fieldsTitle = "";
 foreach (Yii::$app->params['languages'] as $lang) {
     $pref = $lang['url'];
-    $field = $fieldCode . "[" . $lang['url'] . "]";
+    $field = $fieldLabel . "[" . $lang['url'] . "]";
+    $field_name = $fieldCode . "[" . $lang['url'] . "]";
     $value = (isset($model->title[$pref])) ? $model->title[$pref] : "";
     $col_md = "col-md-" . (int)(12 / count(Yii::$app->params['languages']));
     $fieldsTitle .= '
 <div class="form-group field-seo-title ' . $col_md . '">
     <label class="control-label" for="seo-title">' . $field . '</label>
-    <textarea id="seo-title" class="form-control" name="' . $field . '" maxlength="255" rows="6" aria-invalid="false">' . $value . '</textarea>
+    <textarea id="seo-title" class="form-control" name="' . $field_name . '" maxlength="255" rows="6" aria-invalid="false">' . $value . '</textarea>
 </div>';
 }
 ?>
 
 <?php
-$fieldCode = "Description";
+$fieldCode = "seo_description";
+$fieldLabel = "SEO Description";
 $fieldsDescription = "";
 foreach (Yii::$app->params['languages'] as $lang) {
     $pref = $lang['url'];
-    $field = $fieldCode . "[" . $lang['url'] . "]";
+    $field = $fieldLabel . "[" . $lang['url'] . "]";
+    $field_name = $fieldCode . "[" . $lang['url'] . "]";
     $value = (isset($model->description[$pref])) ? $model->description[$pref] : "";
     $col_md = "col-md-" . (int)(12 / count(Yii::$app->params['languages']));
     $fieldsDescription .= '
 <div class="form-group field-seo-description ' . $col_md . '">
     <label class="control-label" for="seo-description">' . $field . '</label>
-    <textarea id="seo-description" class="form-control" name="' . $field . '" maxlength="255" rows="6" aria-invalid="false">' . $value . '</textarea>
+    <textarea id="seo-description" class="form-control" name="' . $field_name . '" maxlength="255" rows="6" aria-invalid="false">' . $value . '</textarea>
 </div>';
 }
 ?>
-
+<?php
+$fieldCode = "seo_article_title";
+$fieldLabel = "SEO Заголовок статьи";
+$fieldsArticleTitle = "";
+foreach (Yii::$app->params['languages'] as $lang) {
+    $pref = $lang['url'];
+    $field = $fieldLabel . "[" . $lang['url'] . "]";
+    $field_name = $fieldCode . "[" . $lang['url'] . "]";
+    $value = (isset($model->article_title[$pref])) ? $model->article_title[$pref] : "";
+    $col_md = "col-md-" . (int)(12 / count(Yii::$app->params['languages']));
+    $fieldsArticleTitle .= '
+<div class="form-group field-seo-article-title ' . $col_md . '">
+    <label class="control-label" for="seo-article-title">' . $field . '</label>
+    <input type="text" id="seo-article-title" class="form-control" name="' . $field_name . '" value="' . $value . '">
+</div>';
+}
+?>
+<?php
+$fieldCode = "seo_article_body";
+$fieldLabel = "SEO статья";
+$fieldsArticleBody = "";
+foreach (Yii::$app->params['languages'] as $lang) {
+    $pref = $lang['url'];
+    $field = $fieldLabel . "[" . $lang['url'] . "]";
+    $field_name = $fieldCode . "[" . $lang['url'] . "]";
+    $value = (isset($model->article_body[$pref])) ? $model->article_body[$pref] : "";
+    $col_md = "col-md-" . (int)(12 / count(Yii::$app->params['languages']));
+    $fieldsArticleBody .= '
+<div class="form-group field-seo-article-body ' . $col_md . '">
+    <label class="control-label" for="seo-article-body">' . $field . '</label>
+    <textarea id="seo-article-body" class="form-control" name="' . $field_name . '" maxlength="512" rows="10" aria-invalid="false">' . $value . '</textarea>
+</div>';
+}
+?>
 
 <div class="seo-form">
 
@@ -58,6 +95,12 @@ foreach (Yii::$app->params['languages'] as $lang) {
     </div>
     <div class="row">
         <?= $fieldsDescription ?>
+    </div>
+    <div class="row">
+        <?= $fieldsArticleTitle ?>
+    </div>
+    <div class="row">
+        <?= $fieldsArticleBody ?>
     </div>
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
---
+++ console/migrations/m211104_112211_add_seo_article.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211104_112211_add_seo_article
+ */
+class m211104_112211_add_seo_article extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('seo','article_title', $this->string(255));
+        $this->addColumn('seo','article_body', $this->text());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('seo','article_title');
+        $this->dropColumn('seo','article_body');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m211104_112211_add_seo_article cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
