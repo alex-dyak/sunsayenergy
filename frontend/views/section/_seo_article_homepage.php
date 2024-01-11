1fd905ba471912e37303c0d0080ed6287fc11916
Julia Karpenko (Hyperion) <julia.karpenko@oddscheckerglobalmedia.com>
2024-01-11 21:30:38 +0200

    added text for btn

--- frontend/views/section/_seo_article_homepage.php
+++ frontend/views/section/_seo_article_homepage.php
@@ -5,8 +5,78 @@
             <div class="seo-article-inner">
                 <p><?= $seo_data['article_body'][Yii::$app->language] ?></p>
             </div>
-            <span class="seo-article-opened"></span>
+            <span class="seo-article-opened" data-open="<?= BaseController::getMessage('865') ?>" data-close="<?= BaseController::getMessage('866') ?>"></span>
         </div>
     </div>
 <?php endif; ?>
 
+
+<p>
+    Autonomous solar power stations: stable energy supply and obvious savings
+
+    Autonomous solar power stations are a highly efficient alternative source of electricity. These power stations work
+    by using sunlight, which is an endless natural source of energy, and turning it into the electricity we need for our
+    everyday use.
+
+    When does it make sense to consider installing an autonomous solar power station?
+
+    A custom-made autonomous solar power station can solve multiple problems:
+
+    Absence or distance from a regular power grid:
+    If you don't have access to a traditional electricity grid or it's far away, an autonomous solar power station can
+    provide reliable electricity during the day, ensuring your comfort.
+
+    Frequent power outages:
+    Traditional power grids often suffer from power supply interruptions, causing inconvenience. In contrast, autonomous
+    solar power stations operate continuously without such interruptions. These stations are equipped with monitoring
+    systems that detect and address minor malfunctions promptly, ensuring a smooth and uninterrupted power supply.
+
+    Insufficient power capacity of the power grid:
+    If the power grid in your area has limited capacity and struggles to meet your energy needs, solar power stations
+    with batteries can come to the rescue. These stations are designed to supplement your energy requirements, and with
+    careful planning and system design, they can provide additional capacity as needed.
+
+    Investing in a solar power station can lead to significant savings on utility bills and reduce dependence on rising
+    tariffs. The cost of such stations depends on the number of solar panels and additional equipment. Typically, these
+    eco-friendly systems cover their costs in around 5 years, providing energy independence and immediate benefits.
+
+
+    You can purchase an independent solar power station for a private house, summer cottage, boarding house, hotel, or
+    farm, which can cover a significant portion of your expenses over time. The only current drawback is the
+    bureaucratic complexity of connecting these stations to the ""Green Tariff"" program. However, it is promised that
+    this will change soon, making hybrid autonomous stations more advantageous.
+
+    Principle of Operation of an Independent Solar Power Station
+
+    An autonomous power station is customized and equipped based on an individual project, considering operating
+    conditions and the required power specifications.
+
+    The list of components includes:
+
+    Solar panels.
+    - Modules capture solar energy, convert it into electricity, and distribute it to power-consuming areas.
+    - Polycrystalline or monocrystalline panels are installed on the southern side of the roof or another suitable
+    surface and connected into a single chain. The frame is made of anodized aluminum, ensuring strength for at least 20
+    years.
+
+    Battery batteries.
+    - Required for storing excess electricity generated during the day. These batteries activate at night or during
+    adverse weather conditions when energy supply decreases.
+
+    Charge controllers.
+    - Special devices that optimize the electricity transfer process from the battery block and control battery
+    charging. Controllers minimize possible losses, allowing the energy storage elements to continue working
+    efficiently.
+
+    Inverters.
+    - Special equipment that serves multiple functions.
+    - The primary inverter converts the direct current produced by the solar module block into alternating current with
+    a voltage suitable for electrical equipment.
+    - Additional inverters are connected to the batteries. The number of devices depends on the number of phases and the
+    nominal load.
+
+    You can purchase a complete autonomous solar power station with comprehensive services from SUNSAY NRG. Their
+    experts, who have a deep understanding of alternative power supply, offer project support, from studying the
+    features of the location to connecting a station that fully meets the client's needs.
+
+</p>
