ce99ddb62e665c7512491dee6b85b7c54b60d206
Julia Karpenko (Hyperion) <julia.karpenko@oddscheckerglobalmedia.com>
2025-04-28 18:29:43 +0300

    updated nav

--- frontend/views/layouts/new_homepage.php
+++ frontend/views/layouts/new_homepage.php
@@ -196,7 +196,8 @@ $contacts = \backend\models\Contact::getContact();
                             <a href="<?= \yii\helpers\Url::to(['/commercial_stations']); ?>"><?= BaseController::getMessage('531') ?></a>
                         </li>
                         <li class="<?= (Yii::$app->controller->route == 'site/reserve') ? 'active' : ''; ?>">
-                            <a href="<?= \yii\helpers\Url::to(['/solar-power-for-backup-power']); ?>"><?= BaseController::getMessage('897') ?></a>
+                            <a class="hidden-mobile" href="<?= \yii\helpers\Url::to(['/solar-power-for-backup-power']); ?>"><?= BaseController::getMessage('897') ?></a>
+                            <a class="visible-mobile" href="<?= \yii\helpers\Url::to(['/solar-power-for-backup-power']); ?>"><?= BaseController::getMessage('898') ?></a>
                         </li>
                         <?php if (stripos($_SERVER['REQUEST_URI'], '/en/') === false) { ?>
                             <li class="subnav"><?= BaseController::getMessage('12') ?></li>
